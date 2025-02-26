4cd018b954abe841aac00f6a195a84280a015d72
Prateek <prateekjain6342@gmail.com>
2025-02-26 20:21:46 +0530

    Changed UI

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,7 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-// import NextTopLoader from 'nextjs-toploader';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const jakarta_sans = Plus_Jakarta_Sans({
   variable: "--font-plusjakarta",
@@ -10,13 +10,14 @@ const jakarta_sans = Plus_Jakarta_Sans({
 })
 
 export const metadata = {
-  title: "Splitzer",
-  description: "Bill Splitting made easy",
+  title: "Splitzer - Split expenses for shared expenses",
+  description: "An easy, reliable and quick tool to split your expenses as a group after any outings in any ratio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${jakarta_sans.variable} scroll-smooth`}>
+      <GoogleAnalytics gaId="G-TJGLJ53HSG" />
       <body className="">
         <Header />
         {children}
